293937af48331eace84bb13bbc3507083a394149
syzelda <115057595+syzelda@users.noreply.github.com>
2022-12-08 23:46:02 +0800

    It is odd of 1000~1500.

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int range_Max =1500;
+	int range_Min =1000;
+	int number ; 
+	int i;
+	int k;
+	int j;
+	
+	k = 1000 / 2 ;
+	number = k * 2 - 1;
+	j=(range_Max - range_Min) / 2 ; //執行次數 
+	
+	  for(i=0; i < j; i++){
+	  	if(number > 1000 || number < 1500) //1000~1500的範圍 
+	  	number += 2;      
+	  	printf("%d\n",number);
+	  }
+	return 0;
+}
